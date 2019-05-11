a8a0dbf6ee28889612d02ca3f096d09725f0cb0c
Haisu Huang <haisuh@uoregon.edu>
2019-05-11 14:13:54 -0700

    adding wdi data to R

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -1,6 +1,6 @@
 #' ---
 #' title: "organize_data.R"
-#' author: ""
+#' author: "Haisu Huang"
 #' ---
 
 # This script will read in raw data from the input directory, clean it up to produce 
@@ -8,3 +8,15 @@
 
 #source in any useful functions
 source("useful_functions.R")
+library(readr)
+
+# creating dataset in R
+wdidata <- read_csv("analysis/input/WDI_Data.csv", 
+                    col_names = c("counname", "councode", "time", "timecode",
+                                  "pm2.5", "mortrate", "mortdisease","lifexpec","co2emmission"),
+                    skip = 1)
+wdidef <- read_csv2("analysis/input/WDI_Definition and Source.csv")
+
+# saving dataset to output folder
+write_csv(wdidata, "analysis/input/WDI_Data.csv")
+write_csv(wdidef, "analysis/input/WDI_Definition and Source.csv")
